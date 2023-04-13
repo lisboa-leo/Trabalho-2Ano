a45ac297cf1aa458135c9189117b09f828c6a1df
lisboa-leo <leonardo.lisboa.silva.10@gmail.com>
2023-04-13 16:58:49 -0300

    pop-up

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var menu = document.getElementById("lista-navbar");
 var msgCookies = document.getElementById('cookies-msg');
+var botao = document.getElementById('submit');
 
     function abrirmenu(){
         menu.style.right = "0"
@@ -7,7 +8,7 @@ var msgCookies = document.getElementById('cookies-msg');
     function fecharmenu(){
         menu.style.right = "-200px"
     }
-    
+
 
 function aceito(){
     localStorage.AceitoLgpd = "Aceito"
@@ -16,15 +17,40 @@ function aceito(){
 }
 function naoaceitocookies(){
     localStorage.NaoAceitoLgpd = "Nao Aceito"
-    msgCookies.classList.add('mostrar')
+    msgCookies.classList.remove('mostrar')
 }
+
+
 if(localStorage.AceitoLgpd == 'Aceito'){
     msgCookies.classList.remove('mostrar')
 }
+
+else if(localStorage.NaoAceitoLgpd == 'Nao Aceito'){
+    msgCookies.classList.remove('mostrar')
+    botao.disabled=true
+    alert("Você precisa aceitar o cookies para enviar o email")
+}
+
 else{
     msgCookies.classList.add('mostrar')
 }
 
+
 function voltar(){
     window.history.back()
+}
+
+function enviarEmail(){
+    Email.send({
+        SecureToken : "56cb31ea-646d-427c-ba71-77cb6fd21567",
+        To : "leonardolisboalsl007@gmail.com",
+        From : "leonardolisboalsl007@gmail.com",
+        Subject : "Entrando em contato pelo Site da Working at Var",
+        Body : "<b>Nome:</b> " + document.getElementById("nome").value
+            + "<br> <br> <b>Email:</b> " + document.getElementById("email-usuario").value
+            + "<br> <br> <b>Assunto:</b>: " + document.getElementById("assunto").value
+            + "<br> <br> <b>Mensagem:</b> " + document.getElementById("mensagem").value
+        }).then(
+        message => alert("mensagem enviada com sucesso!")
+        );
 }
